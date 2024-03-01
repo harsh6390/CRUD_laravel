28bf14a4d8603d8661c82a9a4d216e04f70227f9
Harsh Srivastava <119413903+harsh6390@users.noreply.github.com>
2024-03-01 13:53:52 +0530

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="google-site-verification" content="_RaBjuXhMd6_imf45wyhMxBpYr3AneCZQWxnG0F7BB4" />
+   <meta name="google-site-verification" content="_RaBjuXhMd6_imf45wyhMxBpYr3AneCZQWxnG0F7BB4" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Create</title>
 </head>
